a77f0a72d990dd9dcd74209cf5d39dd8a3fb3b0c
pola0104 <pola0111.0444@gmail.com>
2026-03-04 16:13:27 +0800

    Show test-not-grant you from error principle

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -1,10 +1,20 @@
 import sys
 sys.path.append("../src")
 #TODO make it with `pip install -e .`
-from math_demo import add
+from math_demo import (
+    add,
+    add_with_bug
+)
 
 def test_addition():
-    assert add(2, 2) == 4
+    assert add(2, 2) == 4, "Function did not return 4"
     print("Test BASIC ADDITION")
+
+def test_addition_with_bug():
+    assert add_with_bug(2, 2) == 4, "Function did not return 4"
+    assert add_with_bug(0, 0) == 0
+    print("Test BASIC ADDITION PASSED (does it mean code ok?)")
+    #assert add_with_bug(6, 7) == 13 #will fail here
 if __name__ == "__main__":
     test_addition()
+    test_addition_with_bug()
