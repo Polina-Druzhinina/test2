c539b6b29dece9f84493acfce7319c0f831a2257
pola0104 <pola0111.0444@gmail.com>
2026-03-04 16:41:07 +0800

    Added test for reasonable ammouth of test cases and for logically separated

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -28,8 +28,24 @@ def test_addition_overcomplicated():
             assert add(-i, -j) == sum([-i,-j])
             assert add(-i, j) == sum([-i,j])
             assert add(i, -j) == sum([i,-j])
+
+def test_addition_reasonable():
+    assert add(2, 2) == 4
+    assert add(0, 0) == 0
+    assert add(6, 7) == 13 #will fail here
+    assert add(6, -7) == -1
+    assert add(-7, 0) == -7
+    assert add(7, 0) == 7
+    print("Test ADDITION REASONABLE PASS")
+
+def test_addition_commutative():
+    #can be in previous test but logically separated
+    assert add(7,-6) == 1
+    assert add(-6,7) == 1
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
     test_addition_dublicated()
-    #test_addition_overcomplicated() 
+    #test_addition_overcomplicated()
+    test_addition_reasonable()
+    test_addition_commutative()
