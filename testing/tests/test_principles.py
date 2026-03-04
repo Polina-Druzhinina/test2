2ea28fbeb77b31c29cafe7b4855e1f8df119fcbf
pola0104 <pola0111.0444@gmail.com>
2026-03-04 16:20:37 +0800

    Showed do-not-dublicated (do-not-rely-on) logic of tests function

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -15,6 +15,12 @@ def test_addition_with_bug():
     assert add_with_bug(0, 0) == 0
     print("Test BASIC ADDITION PASSED (does it mean code ok?)")
     #assert add_with_bug(6, 7) == 13 #will fail here
+
+#дублирование логики
+def test_addition_dublicated():
+    assert add(2,3) == 2+3 #предполагается что этот плюс не используется в той функции
+
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
+    test_addition_dublicated()
