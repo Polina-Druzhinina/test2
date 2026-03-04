db71b74e74c0636447bbf45afe12889894a74041
pola0104 <pola0111.0444@gmail.com>
2026-03-04 16:29:22 +0800

    Show redundant test principles

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -20,7 +20,16 @@ def test_addition_with_bug():
 def test_addition_dublicated():
     assert add(2,3) == 2+3 #предполагается что этот плюс не используется в той функции
 
+def test_addition_overcomplicated():
+    #formal valid test but too slow
+    for  i in range(0, 2**32):
+        for j in range(0, 2**32): 
+            assert add(i,j) == sum([i, j])  #не должны использовать все наборы входных параметров
+            assert add(-i, -j) == sum([-i,-j])
+            assert add(-i, j) == sum([-i,j])
+            assert add(i, -j) == sum([i,-j])
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
     test_addition_dublicated()
+    #test_addition_overcomplicated() 
