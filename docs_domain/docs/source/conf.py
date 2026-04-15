6b22d2e4c43182a92a42b2e31ea9e1facec4bd40
pola0104 <pola0111.0444@gmail.com>
2026-04-15 16:22:55 +0800

    Enable Markdown and reStructuredText support in documentation

--- docs_domain/docs/source/conf.py
+++ docs_domain/docs/source/conf.py
@@ -14,8 +14,14 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "myst_parser",
+]
 
+sourse_suffix = {
+    ".md":"markdown",
+    ".rst": "restructuredtext",
+}
 templates_path = ['_templates']
 exclude_patterns = []
 
