052566d29964ee2c3561cf8bdebbea59e131d00e
pola0104 <pola0111.0444@gmail.com>
2026-06-06 17:14:20 +0800

    changed the description to Redmi and updated the version

--- task-tracker/setup.py
+++ task-tracker/setup.py
@@ -5,8 +5,8 @@ from setuptools import(
 
 setup(
     name="task-tracker-cli-dev",
-    version="0.1.1",
-    long_description="Task tracker application",
+    version="0.1.2",
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url = "https://github.com/Polina-Druzhinina/test2/tree/feature/testpypi/task-tracker",
     package_dir={"":"src"},
